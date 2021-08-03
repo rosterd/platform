db942d7a31e3af1c7128806553da24fb261740ce
ricky <ricky.gummadi>
2021-08-04 00:24:10 +1200

    wip user admin contoller

--- src/backend/Rosterd.Services.Auth/Interfaces/IRolesService.cs
+++ src/backend/Rosterd.Services.Auth/Interfaces/IRolesService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Auth0.AuthenticationApi;
 using Auth0.ManagementApi;
 using Microsoft.Extensions.Options;
+using Rosterd.Domain.Enums;
 using Rosterd.Domain.Models.Roles;
 using Rosterd.Domain.Settings;
 
@@ -13,6 +14,8 @@ namespace Rosterd.Services.Auth.Interfaces
         Task<List<RosterdRole>> GetAllRoles();
 
         Task<RosterdRole> GetRole(string roleId);
+
+        Task<RosterdRole> GetRole(RosterdRoleEnum rosterdRoleEnum);
 
         Task<RosterdRole> AddRole(RosterdRole roleToAdd);
 
