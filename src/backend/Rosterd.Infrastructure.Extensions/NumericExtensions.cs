ff294347afb028d61975bd5197370c3301943f84
Ricky Gummadi <ricky.gummadi@outlook.com>
2021-09-18 21:30:04 +1200

    no message

--- src/backend/Rosterd.Infrastructure.Extensions/NumericExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Rosterd.Infrastructure.Extensions
-{
-    public static class NumericExtensions
-    {
-        public static bool IsLessThanOrEqualToZero(this long value) =>  value <= 0;
-    }
-}
