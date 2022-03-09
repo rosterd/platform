c03183c0814f3f29dec26c9f6112127a8af02538
Ricky <ricky.gummadi@outlook.com>
2022-03-09 22:26:07 +1300

    Upgrade all to .NET 6.0

--- src/backend/Rosterd.Services.Facilities/FacilitiesService.cs
+++ src/backend/Rosterd.Services.Facilities/FacilitiesService.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Rosterd.Data.SqlServer.Context;
